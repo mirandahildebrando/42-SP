3bb24a692ff33131f39315178c3b335244fc83c0
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2026-03-18 00:10:35 -0300

    ReverseAlphabet

---
+++ ft_print_reverse_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hvanique <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/28 16:35:33 by hvanique          #+#    #+#             */
+/*   Updated: 2026/03/07 17:17:19 by hvanique         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+} */
